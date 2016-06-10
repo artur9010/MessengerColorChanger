ef45102d64f42d4bb1ad2058956f012074cd6404
Artur <arturmotyka99@gmail.com>
2016-06-10 20:17:08 +0200

    still no working xD

--- app.py
+++ app.py
@@ -1,78 +1,102 @@
-from PySide.QtUiTools import QUiLoader
 from messenger.MessengerAPI.Messenger import Messenger
-import getpass
 from PySide.QtCore import *
 from PySide.QtGui import *
 import sys
-import ui
 from PySide import QtCore, QtGui
 
 
-class MessengerColorChanger(QMainWindow):
+class MessengerColorChanger(QtGui.QWidget):
     def __init__(self, parent=None):
         super(MessengerColorChanger, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi()
 
 
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.pushButton = QtGui.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(17, 250, 361, 34))
-        self.pushButton.setObjectName("pushButton")
-        self.login = QtGui.QLineEdit(Dialog)
-        self.login.setGeometry(QtCore.QRect(120, 20, 261, 32))
-        self.login.setMaxLength(128)
-        self.login.setObjectName("login")
-        self.label = QtGui.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(20, 30, 58, 18))
-        self.label.setObjectName("label")
-        self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(20, 80, 58, 18))
-        self.label_2.setObjectName("label_2")
-        self.haslo = QtGui.QLineEdit(Dialog)
-        self.haslo.setGeometry(QtCore.QRect(120, 70, 261, 32))
-        self.haslo.setMaxLength(64)
-        self.haslo.setEchoMode(QtGui.QLineEdit.Password)
-        self.haslo.setObjectName("haslo")
-        self.id = QtGui.QLineEdit(Dialog)
-        self.id.setGeometry(QtCore.QRect(120, 120, 261, 32))
-        self.id.setMaxLength(30)
-        self.id.setObjectName("id")
-        self.label_3 = QtGui.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(20, 130, 81, 18))
-        self.label_3.setObjectName("label_3")
-        self.kolor = QtGui.QLineEdit(Dialog)
-        self.kolor.setEnabled(True)
-        self.kolor.setGeometry(QtCore.QRect(120, 170, 261, 32))
-        self.kolor.setMaxLength(8)
-        self.kolor.setObjectName("kolor")
-        self.label_4 = QtGui.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(20, 180, 81, 18))
-        self.label_4.setObjectName("label_4")
+    def setupUi(self):
+        #Todo: lista konf
+        #todo: szukajka konf
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        # login
+        login = QtGui.QLineEdit()
+        login.setMaxLength(128)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(
-            QtGui.QApplication.translate("Dialog", "Messenger Color Changer", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "Zmień", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Dialog", "Login", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("Dialog", "Hasło", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("Dialog", "ID rozmowy", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("Dialog", "Nowy kolor", None, QtGui.QApplication.UnicodeUTF8))
+        login_label = QtGui.QLabel("Login")
 
-    def go(self, login, password, id, color):
-        messenger = Messenger(login, password)
-        konfa = messenger.get_thread(int(id))
-        konfa.set_custom_color(color)
+        grid.addWidget(login_label, 0, 0)
+        grid.addWidget(login, 0, 1)
 
-if __name__ == '__main__':
+        # password
+        #TODO: "pokaz haslo"
+        #TODO: zapamietaj haslo
+        password = QtGui.QLineEdit()
+        password.setMaxLength(64)
+        password.setEchoMode(QtGui.QLineEdit.Password)
+
+        password_label = QtGui.QLabel("Hasło")
+
+        grid.addWidget(password_label, 1, 0)
+        grid.addWidget(password, 1, 1)
+
+        # id
+        id = QtGui.QLineEdit()
+        id.setMaxLength(30)
+
+        id_label = QtGui.QLabel("ID rozmowy")
+
+        grid.addWidget(id_label, 2, 0)
+        grid.addWidget(id, 2, 1)
+
+        # color
+        #todo: color picker
+        color = QtGui.QLineEdit()
+        color.setMaxLength(8)
+
+        color_label = QtGui.QLabel("Kolor")
+
+        grid.addWidget(color_label, 3, 0)
+        grid.addWidget(color, 3, 1)
+
+        # button
+        button = QtGui.QPushButton()
+        button.setText("Do dzieła!")
+        #button.clicked.connect(go(login.text, password.text, id.text, color.text))
+        #button.clicked.connect(syf("osiem"))
+        grid.addWidget(button, 4, 0, 1, 0)
+
+
+        # window
+        self.setLayout(grid)
+
+        self.setGeometry(600, 300, 600, 400) #polozeniex, polozeniey, x, y
+        self.setWindowTitle("Messenger Color Changer")
+        self.show()
+
+
+def syf(text):
+    print(text)
+
+
+def go(login, password, id, color):
+    if not bool(str(login).strip()):
+        return
+    if not bool(str(password).strip()):
+        return
+    if not bool(str(id).strip()):
+        return
+    if not bool(str(color).strip()):
+        return
+    messenger = Messenger(login, password)
+    konfa = messenger.get_thread(int(id))
+    konfa.set_custom_color(color)
+
+def main():
     app = QApplication(sys.argv)
     frame = MessengerColorChanger()
-    frame.show()
     app.exec_()
+
+if __name__ == '__main__':
+    main()
 
 #haslo = getpass.getpass()
 #messenger = Messenger("arturmotyka99@gmail.com", haslo)
