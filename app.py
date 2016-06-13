1b855b4d662ced0f8073f25f89a8ba60dc800fab
Artur <arturmotyka99@gmail.com>
2016-06-13 16:49:08 +0200

    Dat Commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from messenger.MessengerAPI.Messenger import Messenger
+from PySide import QtGui
 from PySide.QtCore import *
 from PySide.QtGui import *
 import sys
-from PySide import QtCore, QtGui
 
 
 class MessengerColorChanger(QtGui.QWidget):
@@ -14,17 +14,21 @@ class MessengerColorChanger(QtGui.QWidget):
         # Todo: lista konf
         # todo: szukajka konf
         grid = QtGui.QGridLayout()
-        grid.setSpacing(10)
+        grid.setSpacing(20)
+
+        self.infobox = QtGui.QLineEdit()
+        self.infobox.setEnabled(False)
+
+        grid.addWidget(self.infobox, 0, 0, 1, 0)
 
         # login
         self.login = QtGui.QLineEdit()
         self.login.setMaxLength(128)
-        self.login.setText("")
 
         login_label = QtGui.QLabel("Login")
 
-        grid.addWidget(login_label, 0, 0)
-        grid.addWidget(self.login, 0, 1)
+        grid.addWidget(login_label, 1, 0)
+        grid.addWidget(self.login, 1, 1)
 
         # password
         # TODO: "pokaz haslo"
@@ -35,35 +39,35 @@ class MessengerColorChanger(QtGui.QWidget):
 
         password_label = QtGui.QLabel("Hasło")
 
-        grid.addWidget(password_label, 1, 0)
-        grid.addWidget(self.password, 1, 1)
+        grid.addWidget(password_label, 2, 0)
+        grid.addWidget(self.password, 2, 1)
 
         # id
         self.id = QtGui.QLineEdit()
-        self.id.setMaxLength(30)
 
         id_label = QtGui.QLabel("ID rozmowy")
 
-        grid.addWidget(id_label, 2, 0)
-        grid.addWidget(self.id, 2, 1)
+        grid.addWidget(id_label, 3, 0)
+        grid.addWidget(self.id, 3, 1)
 
         # color
         # todo: color picker
         self.color = QtGui.QLineEdit()
         self.color.setMaxLength(8)
+        #self.color = QtGui.QColorDialog()
 
         color_label = QtGui.QLabel("Kolor")
 
-        grid.addWidget(color_label, 3, 0)
-        grid.addWidget(self.color, 3, 1)
+        grid.addWidget(color_label, 4, 0)
+        grid.addWidget(self.color, 4, 1)
 
         # button
         self.button = QtGui.QPushButton()
         self.button.setText("Do dzieła!")
         print("debug 1")
-        self.button.clicked.connect(button_click)
+        self.button.clicked.connect(self.button_click)
         print("debug 2")
-        grid.addWidget(self.button, 4, 0, 1, 0)
+        grid.addWidget(self.button, 5, 0, 1, 0)
 
         global instance
         instance = self
@@ -76,37 +80,49 @@ class MessengerColorChanger(QtGui.QWidget):
         self.show()
 
 
-def button_click():
-    print("debug start")
-    print(instance.login.text())
-    print("debug 3")
-    #print(instance.password.text())
-    print("debug 4")
-    print(instance.id.text())
-    print("debug 5")
-    print(instance.color.text())
-    print("debug 6")
-    if not go(instance.login.text(), instance.password.text(), instance.id.text(), instance.color.text()):
-        print("debug = brakuje czegos")
-    else:
-        print("debug = ok")
-    print("debug end")
-    print("")
+    def button_click(self):
+        instance.infobox.setText("Sprawdzam dane...")
+        print("debug start")
+        print(instance.login.text())
+        print("debug 3")
+        #print(instance.password.text())
+        print("debug 4")
+        print(instance.id.text())
+        print("debug 5")
+        print(instance.color.text())
+        print("debug 6")
+        if not self.go(instance.login.text(), instance.password.text(), instance.id.text(), instance.color.text()):
+            print("debug = brakuje czegos")
+            instance.infobox.setText("Któreś z poniższych pól jest puste lub dane są nieprawidłowe!")
+            QMessageBox.critical(self, "Błędzik", "Któreś z powyższych pól jest puste lub dane są nieprawidłowe.", QMessageBox.Ok)
+        else:
+            print("debug = ok")
+            instance.infobox.setText("Kolor zmieniony!")
+        print("debug end")
+        print("")
 
-
-def go(login, password, id, color):
-    if not str(login).strip():
-        return False
-    if not str(password).strip():
-        return False
-    if not str(id).strip():
-        return False
-    if not str(color).strip():
-        return False
-    messenger = Messenger(login, password)
-    konfa = messenger.get_thread(int(id))
-    konfa.set_custom_color(color)
-    return True
+    def go(self, login, password, id, color):
+        if not str(login).strip():
+            return False
+        if len(str(login)) < 5:
+            return False
+        if not str(password).strip():
+            return False
+        if len(str(password)) < 5:
+            return False
+        if not str(id).strip():
+            return False
+        if not str(color).strip():
+            return False
+        if len(str(color)) < 6:
+            return False
+        try:
+            messenger = Messenger(login, password)
+            konfa = messenger.get_thread(int(id))
+            konfa.set_custom_color(color)
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            return False
 
 
 def main():
